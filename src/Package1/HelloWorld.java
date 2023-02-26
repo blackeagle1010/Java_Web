d87aa1f96e10a8867ee196b6948e135f703461ce
Mehmet <mehmetirvasa10@gmail.com>
2023-02-25 23:54:12 -0800

    Java Web 101 Patika

---
+++ src/Package1/HelloWorld.java
@@ -0,0 +1,17 @@
+package Package1;
+
+public class HelloWorld {
+    public static void main (String[] args){
+        int numberone, numbertwo=2, numberthree;
+        int a = numbertwo;
+        /*Burası birden fazla yorum satırıdır
+         System.out.println(a+a);
+         */
+        /** Javadoc
+         @aythor Mehmet İRVASA
+         @code {@code kullanım örneği}*/
+        System.out.println(a*a);
+        System.out.println("Hello_World\tFreedom");
+    }
+}
+
